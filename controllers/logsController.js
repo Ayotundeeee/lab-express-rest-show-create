2ac918b8bb09ed80a03c40dfac02491b97682deb
AAkinkunmi <aakinkunmi@pursuit.org>
2024-06-25 17:01:34 -0400

    complete basic functionality for 'delete' and 'update'

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -24,4 +24,23 @@ logs.post('/', (req, res) => {
     res.json(logsArray[logsArray.length - 1]);
 })
 
+// delete log at index
+logs.delete('/:arrayIndex', (req, res) => {
+    const { arrayIndex } = req.params;
+    if(logsArray[arrayIndex]) {
+        removedLog = logsArray.splice(arrayIndex, 1);
+        res.status(200).json(removedLog);
+    } else {
+        res.status(404).json({error: "404 Log not found"})
+    }
+})
+
+// update log at index
+logs.put('/:arrayIndex', (req, res) => {
+    const { arrayIndex } = req.params;
+    logsArray[arrayIndex] = req.body
+    res.status(200).json(logsArray[arrayIndex]);
+    
+})
+
 module.exports = logs;
